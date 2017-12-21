d5cbbc29cbc4f363d4af494171e9259fd443ff32
Aurel Ioan Patrutiu <aureldevices@hotmail.com>
2017-12-22 00:52:47 +0100

    Created generals file for assests definitions

---
+++ SFML-App/SFML-App/Definitions.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+#define SPLASH_STATE_SHOW_TIME 5.0
+#define SPLASH_SCENE_BACKGROUND_FILEPATH "Resources/res/SplashState.png"
+#define SPLASH_STATE_SOUND "Resources/audio/SplashState.wav"
+
